eea14905443d78c24adbdeaa424ee2f7b46d6717
Naomi Rodriguez <naomi.rodriguez7@aol.com>
2020-02-11 20:28:35 -0500

    added handleInputchange in line 12

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,4 +1,4 @@
-// your components will all go in this `component` directory.
+//// your components will all go in this `component` directory.
 //// feel free to change this component.js into TodoList.js
 
 
